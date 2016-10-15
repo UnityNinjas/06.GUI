2b73f949e3fb757a967af0df4f2c226e9d957501
kasskata <kassskata@gmail.com>
2016-10-15 20:38:19 +0300

    initial commit. Add new GUI logic - Audio Player

---
+++ Assets/Scripts/HUD/MediaPlayer.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class MediaPlayer : MonoBehaviour
+{
+    private readonly Color32 disabledShuffleColor = new Color32(188, 188, 188, 255);
+
+    public Slider progresBar;
+    public Text songName;
+    public Image shuffleImg;
+
+    public AudioClip[] clips;
+    public byte currentSongIndex;
+    public bool wantShuffle;
+    private bool isPlaying;
+
+    public Image playImg;
+    public Sprite playSprite;
+    public Sprite pauseSprite;
+
+
+    //Problems with OnChange on Slider. To work properly need to implement self slider.
+    //public void Update()
+    //{
+    //    if (this.isPlaying)
+    //    {
+    //        this.progresBar.value = SoundManager.instance.musicSource.time;
+    //    }
+    //}
+
+    public void TogglePlaying()
+    {
+        if (this.isPlaying)
+        {
+            Pause();
+            this.playImg.sprite = this.playSprite;
+        }
+        else
+        {
+            Play();
+            this.playImg.sprite = this.pauseSprite;
+        }
+    }
+
+    public void Play()
+    {
+        this.isPlaying = true;
+        SoundManager.instance.MusicPlay(this.clips[this.currentSongIndex]);
+        this.progresBar.maxValue = this.clips[this.currentSongIndex].length;
+        this.songName.text = this.clips[this.currentSongIndex].name;
+    }
+
+    public void Pause()
+    {
+        this.isPlaying = false;
+        SoundManager.instance.musicSource.Pause();
+    }
+
+    public void Next()
+    {
+        if (this.wantShuffle)
+        {
+            byte newSongIndex;
+            do
+            {
+                newSongIndex = (byte)Random.Range(0, this.clips.Length);
+            }
+            while (newSongIndex == this.currentSongIndex);
+
+            this.currentSongIndex = newSongIndex;
+        }
+        else
+        {
+            if (this.currentSongIndex + 1 >= this.clips.Length)
+            {
+                this.currentSongIndex = 0;
+            }
+            else
+            {
+                this.currentSongIndex += 1;
+            }
+        }
+
+        Play();
+        this.progresBar.value = 0;
+    }
+
+    public void GoToTime(Slider slider)
+    {
+        SoundManager.instance.GoToTime(slider.value);
+    }
+
+    public void ToggleShuffle()
+    {
+        this.wantShuffle = !this.wantShuffle;
+        if (this.wantShuffle)
+        {
+            this.shuffleImg.color = Color.white;
+        }
+        else
+        {
+            this.shuffleImg.color = this.disabledShuffleColor;
+        }
+    }
+}
---
+++ Assets/Scripts/MainSceneController/MainController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityStandardAssets.CrossPlatformInput.PlatformSpecific;
+
+public class MainController : MonoBehaviour
+{
+    public StandaloneInputModule standaloneInput;
+
+    [Header("Level 2")]
+    public Transform door;
+    public Light lightOnDoor;
+    public Collider2D colliderOnDoor;
+    public ParticleSystem hackEffect;
+    [Space(10)]
+    [Header("Level 3")]
+    public GameObject laserOnDoor;
+
+    public Transform finishDoor;
+    public Collider2D colliderOnFinishDoor;
+    public ParticleSystem hackEffectFinish;
+
+    public void Start()
+    {
+        SoundManager.instance.MusicPlay(Clip.Rain);
+        Object.DontDestroyOnLoad(SoundManager.instance);
+    }
+
+    public void Update()
+    {
+        if (Input.GetButtonUp("Cancel"))
+        {
+            Hud.instance.ToggleEscapeMenu();
+        }
+    }
+
+    public void OnDoorLockerClicked()
+    {
+        this.hackEffect.Emit(2);
+        StartCoroutine(OpenDoor(this.door, this.colliderOnDoor));
+    }
+
+    public void OnFinishDoorLockerClicked()
+    {
+        StartCoroutine(OpenDoor(this.finishDoor, this.colliderOnDoor));
+        this.colliderOnFinishDoor.isTrigger = true;
+        this.hackEffectFinish.Emit(2);
+    }
+
+    private IEnumerator OpenDoor(Transform door, Collider2D collider)
+    {
+        while (door.rotation.y > 0)
+        {
+            door.Rotate(new Vector3(0, -5f));
+            yield return new WaitForEndOfFrame();
+        }
+
+        collider.enabled = false;
+        this.lightOnDoor.color = Color.green;
+    }
+}
